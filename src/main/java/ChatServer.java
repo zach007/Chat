79b86e4f18be24c6ec875ce17a29fa47a6f7a70d
zach007 <zach007cn@gmail.com>
2018-01-06 13:08:28 +0800

    Socket通信, 1Client 1 Server,传输控制台消息

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class ChatServer {
+public class ChatServer implements Runnable {
 
   private Socket socket = null;
   private ServerSocket server = null;
   private DataInputStream streamIn = null;
+  private Thread thread = null;
 
 
   /**
@@ -21,23 +22,40 @@ public class ChatServer {
       System.out.println("Binding to port " + port + ", please wait  ...");
       server = new ServerSocket(port);
       System.out.println("Server started: " + server);
-      System.out.println("Waiting for a client ...");
-      socket = server.accept();
-      System.out.println("Client accepted: " + socket);
-      open();
-      boolean done = false;
-      while (!done) {
-        try {
-          String line = streamIn.readUTF();
-          System.out.println(line);
-          done = line.equals(".bye");
-        } catch (IOException ioe) {
-          done = true;
-        }
-      }
-      close();
+      start();
     } catch (IOException ioe) {
       System.out.println(ioe);
+    }
+  }
+
+  public void run() {
+    while (thread != null) {
+      try {
+        System.out.println("Waiting for a client ...");
+        socket = server.accept();
+        System.out.println("Client accepted: " + socket);
+        open();
+        boolean done = false;
+        while (!done) {
+          try {
+            String line = streamIn.readUTF();
+            System.out.println(line);
+            done = line.equals(".bye");
+          } catch (IOException ioe) {
+            done = true;
+          }
+        }
+        close();
+      } catch (IOException ie) {
+        System.out.println("Acceptance Error: " + ie);
+      }
+    }
+  }
+
+  public void start() {
+    if (thread == null) {
+      thread = new Thread(this);
+      thread.start();
     }
   }
 
