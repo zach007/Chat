514b991f151480d12e2eb3007c305e65c215e44e
zach007 <zach007cn@gmail.com>
2018-03-26 23:39:52 +0800

    testing jenkins

--- src/main/java/ChatClient.java
+++ src/main/java/ChatClient.java
@@ -83,7 +83,7 @@ public class ChatClient implements Runnable {
   }
 
   /**
-   * @param msg  testing jenkins testing
+   * @param msg  testing jenkins testing1
    */
   public void handle(String msg) {
     if (msg.equals(".bye")) {
